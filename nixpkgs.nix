df918bb6b85927bc49e7b7dfc600bd25c855e8bb
zimbatm <zimbatm@zimbatm.com>
2018-03-20 12:15:29 +0000

    fix nixpkgs URL

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,6 +1,6 @@
 let
   src = fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs-channels/archive/0e7c9b32817e5cbe61212d47a6cf9bcd71789322.tar.bz2";
+    url = "https://github.com/NixOS/nixpkgs-channels/archive/0e7c9b32817e5cbe61212d47a6cf9bcd71789322.tar.gz";
     sha256 = "1dm777cmlhqcwlrq8zl9q2d87h3p70rclpvq36y43kp378f3pd0y";
   };
 in
